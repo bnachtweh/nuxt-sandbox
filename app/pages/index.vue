527b65442bc7174811829e015013994261a6bced
Berrie Nachtweh <info@ambitiondev.io>
2024-08-01 08:26:32 +0200

    initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { status, data } = useFetch<Todo>(
+  "https://jsonplaceholder.typicode.com/todos/1"
+);
+</script>
+
+<template>
+  <div>
+    <h1>{{ status }}</h1>
+    <div>{{ data?.title }}</div>
+  </div>
+</template>
